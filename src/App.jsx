a882882a8c071d456e3db4b380694dab1c094333
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-14 13:30:08 +0100

    feat: add mobile and larger screens page nav components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import LoadingScreen from "./components/LoadingScreen.jsx";
+import NavBar from "./components/NavBar.jsx";
+import MobileMenu from "./components/MobileMenu.jsx";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
   );
 }
 
