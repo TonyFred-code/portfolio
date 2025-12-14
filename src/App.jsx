735d7d9eca279e29350875f8398d1c60e04d468e
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-14 15:34:53 +0100

    feat: add project section component
    include portfolio ready projects

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 import MobileMenu from "./components/MobileMenu.jsx";
 import Home from "./components/sections/Home.jsx";
 import About from "./components/sections/About.jsx";
+import Projects from "./components/sections/Projects.jsx";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -21,6 +22,7 @@ function App() {
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
+        <Projects />
       </div>
     </>
   );
