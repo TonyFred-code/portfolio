559f6a058733173026d79dc3efa78bc44034bd51
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-14 13:00:32 +0100

    feat: add loading screen component
    add typing  loading animation
    add fadeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import { useState } from "react";
+import LoadingScreen from "./components/LoadingScreen.jsx";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+  );
+}
 
 export default App;
