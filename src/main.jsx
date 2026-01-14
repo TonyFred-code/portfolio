dce2defc7bf7deb2412984e554ea6f2868448f28
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-14 14:29:02 +0100

    feat: add Showcase page path to browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,24 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages/Home.jsx";
 import Error from "./pages/ErrorPage.jsx";
 import ThemeProvider from "./context/ThemeProvider.jsx";
+import Showcase from "./pages/Showcase.jsx";
+import projectsLoader from "./loaders/projectsLoader.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <Error />,
+    loader: projectsLoader,
+    id: "root",
     children: [
       {
         index: true,
         element: <Index />,
+      },
+      {
+        path: "showcase",
+        element: <Showcase />,
       },
     ],
   },
