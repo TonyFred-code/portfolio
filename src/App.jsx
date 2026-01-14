1b9953f09cbe60be3ce475b276c2672e732c8c59
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-01-14 14:27:33 +0100

    feat: show loader comp only on first page load
    move showing loader boolean to sessionStorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,36 @@ import NavBar from "./components/NavBar.jsx";
 import MobileMenu from "./components/MobileMenu.jsx";
 import Footer from "./components/Footer.jsx";
 import { ToastContainer } from "react-toastify";
-import useProjects from "./hooks/useProjects.jsx";
 import { Outlet } from "react-router-dom";
-import ScrollToHashElement from "./components/ScrollToHashElement.jsx";
 import { resolveTheme } from "./helpers/themeUtils.js";
 import useTheme from "./hooks/useTheme.jsx";
+import ScrollToHashElement from "./components/ScrollToHashElement.jsx";
+import NavigationBar from "./components/TopLoader.jsx";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const { theme } = useTheme();
-  const { loading, error } = useProjects();
-  const shouldShowLoader = loading || !isLoaded;
   const toastTheme = resolveTheme(theme);
 
-  if (error) return <div>An error occurred: {error}</div>;
+  const [showLoader, setShowLoader] = useState(() => {
+    const hasShown = sessionStorage.getItem("loaderShown");
+    return !hasShown;
+  });
+
+  function handleLoaderComplete() {
+    sessionStorage.setItem("loaderShown", "true");
+    setShowLoader(false);
+  }
+
+  if (showLoader) {
+    return <LoadingScreen onComplete={handleLoaderComplete} />;
+  }
 
   return (
     <>
+      <NavigationBar />
       <ScrollToHashElement behavior="smooth" />
-      {shouldShowLoader && (
-        <LoadingScreen onComplete={() => setIsLoaded(true)} />
-      )}
-
-      <div
-        className={`min-h-screen transition-opacity duration-700
-          ${!shouldShowLoader ? "opacity-100" : "opacity-0"}
-          bg-background text-foreground
-        `}
-      >
+      <div className={`min-h-screen bg-background text-foreground`}>
         <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Outlet />
